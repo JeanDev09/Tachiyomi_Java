49b46c3ce1d4215f87a94f74fbd097a1efa56d63
JeanDev09 <jean132309@gmail.com>
2024-10-29 11:56:38 -0500

    primer commit

---
+++ src/main/java/eu/kanade/tachiyomi/Tachiyomi.java
@@ -0,0 +1,12 @@
+package eu.kanade.tachiyomi;
+
+/**
+ *
+ * @author jeandev
+ */
+public class Tachiyomi {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
